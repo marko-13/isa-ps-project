63b23c2765b2a41d2ca6df8402c8fe4c41437e0b
bokykisac <dervy97@gmail.com>
2019-11-06 14:26:32 +0100

    Adding db annotation for class Operation

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/Operation.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/Operation.java
@@ -1,25 +1,32 @@
 package com.proj.medicalClinic.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Entity
+@DiscriminatorValue("O")
 public class Operation extends Appointment {
-    private UUID id;
+
+    @ManyToMany
+    @JoinTable(name = "operating", joinColumns = @JoinColumn(name = "operation_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "doctor_id", referencedColumnName = "id"))
     private List<Doctor> doctors;
+
+    @OneToOne
     private Patient patient;
 
     public Operation(){
-
+        super();
     }
 
-    public Operation(UUID id, List<Doctor> doctors, Patient patient) {
-        this.id = id;
+    public Operation(UUID id, Date date, OperationRoom or, Service service, double duration, List<Doctor> doctors, Patient patient) {
+        super(id, date, or, service, duration);
         this.doctors = doctors;
         this.patient = patient;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public void setDoctors(List<Doctor> doctors) {
@@ -28,10 +35,6 @@ public class Operation extends Appointment {
 
     public void setPatient(Patient patient) {
         this.patient = patient;
-    }
-
-    public UUID getId() {
-        return id;
     }
 
     public List<Doctor> getDoctors() {
