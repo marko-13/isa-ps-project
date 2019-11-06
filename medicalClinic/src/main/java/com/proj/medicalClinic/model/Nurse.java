921b3e329723c20c3d1e922655dc34c04531337a
bokykisac <dervy97@gmail.com>
2019-11-06 13:02:19 +0100

    Updating nurse class

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/Nurse.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/Nurse.java
@@ -1,9 +1,36 @@
 package com.proj.medicalClinic.model;
 
-public class Nurse extends MedicalStaff{
+import java.util.List;
+import java.util.UUID;
+
+public class Nurse extends AppUser{
+
+	private List<Leave> leaves;
+	private int shift;
 	
 	public Nurse() {
-		
+		super();
 	}
-	
+
+	public Nurse(UUID id, String email, String password, String name, String lastName, List<Leave> leaves, int shift) {
+		super(id, email, password, name, lastName);
+		this.leaves = leaves;
+		this.shift = shift;
+	}
+
+	public List<Leave> getLeaves() {
+		return leaves;
+	}
+
+	public void setLeaves(List<Leave> leaves) {
+		this.leaves = leaves;
+	}
+
+	public int getShift() {
+		return shift;
+	}
+
+	public void setShift(int shift) {
+		this.shift = shift;
+	}
 }
