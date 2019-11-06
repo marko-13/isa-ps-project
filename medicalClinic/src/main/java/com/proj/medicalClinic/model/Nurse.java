1a3e95a3a82320453717aadf7e3e63385a11ac89
bokykisac <dervy97@gmail.com>
2019-11-06 13:36:45 +0100

    Added db annotaions for class Nurse

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/Nurse.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/Nurse.java
@@ -1,11 +1,18 @@
 package com.proj.medicalClinic.model;
 
+import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
 
+
+@Entity
+@DiscriminatorValue("NR")
 public class Nurse extends AppUser{
 
+	@OneToMany(mappedBy = "id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Leave> leaves;
+
+	@Column(name = "shift", nullable = false)
 	private int shift;
 	
 	public Nurse() {
