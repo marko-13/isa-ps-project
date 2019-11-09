e84931aa3d05bac2de3b8d409edfe1450202b87a
bokykisac <dervy97@gmail.com>
2019-11-09 17:43:37 +0100

    Adding @DiscriminatorValue to class AdminClinic

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/AdminClinic.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/AdminClinic.java
@@ -3,6 +3,7 @@ package com.proj.medicalClinic.model;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -11,9 +12,10 @@ import javax.persistence.ManyToOne;
 @Data
 @SuperBuilder
 @Entity
+@DiscriminatorValue("AC")
 public class AdminClinic extends AppUser {
 
     @ManyToOne
-    @JoinColumn(name = "clinic_id", nullable = false)
+    @JoinColumn(name = "clinic_id", nullable = true)
     private Clinic clinic;
 }
