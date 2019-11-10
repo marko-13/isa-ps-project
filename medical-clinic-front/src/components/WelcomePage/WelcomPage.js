2ff092e99247332b7cd0f44a04cd96c1c32ecf61
bokykisac <dervy97@gmail.com>
2019-11-10 13:47:32 +0100

    Adding bootstrap to forms

--- medical-clinic-front/src/components/WelcomePage/WelcomPage.js
+++ medical-clinic-front/src/components/WelcomePage/WelcomPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 
 import Login from '../../containers/Login/Login';
+import Register from '../../containers/Register/Register';
 
 const WelcomPage = () => {
     return (
         <Auxiliary>
             <Login />
+            <Register />
         </Auxiliary>
     );
 };
--- medical-clinic-front/src/containers/Login/Login.js
+++ medical-clinic-front/src/containers/Login/Login.js
@@ -24,29 +24,39 @@ class Login extends Component {
             .catch(err => {
                 console.log(err);
             });
-        
+
 
     }
 
 
     render() {
         return (
-            <div>
-                
-            <div>
-                <label>Username: </label>
-                <input type='text' onChange={(event) => this.setState({username: event.target.value})}></input>
-            </div>
-
-            <div>
-                <label>Username: </label>
-                <input type='password' onChange={(event) => this.setState({password: event.target.value})}></input>
-            </div>
-
-            <div>
-                <button onClick={this.loginSubmitHandler}>Login</button>
-            </div>
-
+            <div className="container login-container">
+                <div className="row">
+                    <div className="col-md-6 login-form-1">
+                        <h3>User login</h3>
+                            <div className="form-group">
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Your username *"
+                                    value={this.state.username}
+                                    onChange={(event) => this.setState({username: event.target.value})}
+                                    />
+                            </div>
+                            <div className="form-group">
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    placeholder="Your Password *"
+                                    value={this.state.password}
+                                    onChange={(event) => this.setState({password: event.target.value})}/>
+                            </div>
+                            <div className="form-group">
+                                <button  className="btn-primary" onClick={this.loginSubmitHandler}>Login</button>
+                            </div>
+                    </div>
+                </div>
             </div>
         );
     }
