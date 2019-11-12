10e26750778e71b792db7d2c31df53b9e84e7bc6
bokykisac <dervy97@gmail.com>
2019-11-12 22:17:15 +0100

    Adding get all users on click example (get request from backend)

--- medical-clinic-front/src/components/WelcomePage/WelcomePage.js
+++ medical-clinic-front/src/components/WelcomePage/WelcomePage.js
@@ -3,12 +3,15 @@ import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 
 import Login from '../../containers/Login/Login';
 import Register from '../../containers/Register/Register';
+import Users from '../../containers/Users/Users';
 
 const WelcomePage = () => {
+
     return (
         <Auxiliary>
             <Login />
             <Register />
+            <Users />
         </Auxiliary>
     );
 };
