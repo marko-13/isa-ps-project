cea276ffbb977cab7fb9f1f053acd5f8b6d032da
bokykisac <dervy97@gmail.com>
2019-11-12 21:27:42 +0100

    Adding User Service and method findAll

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/AppUserService.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/AppUserService.java
@@ -1,6 +1,21 @@
 package com.proj.medicalClinic.service;
 
-public interface AppUserService {
+import com.proj.medicalClinic.model.AppUser;
+import com.proj.medicalClinic.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppUserService {
+
+    @Autowired
+    private AppUserRepository userRepository;
+
+    public List<AppUser> findAll() {
+        return userRepository.findAll();
+    }
 
 
 }
