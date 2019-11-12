009da1658da44111b09740937830b0a047e1c8e7
bokykisac <dervy97@gmail.com>
2019-11-12 21:27:07 +0100

    Adding User Repository and findAll method

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/AppUserRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/AppUserRepository.java
@@ -2,14 +2,16 @@ package com.proj.medicalClinic.repository;
 
 import com.proj.medicalClinic.model.AppUser;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Pageable;
+import java.util.List;
 import java.util.UUID;
 
 public interface AppUserRepository extends JpaRepository <AppUser, UUID>{
 
-    //Page<AppUser> findAll(Pageable pageable);
+    List<AppUser> findAll();
 
     //AppUser findByEmail(String email);
 
