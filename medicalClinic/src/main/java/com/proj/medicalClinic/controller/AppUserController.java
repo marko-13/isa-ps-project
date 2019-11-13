e34f335e3acd0b7256574151aadd848c749b1f53
marko-13 <50908634+marko-13@users.noreply.github.com>
2019-11-13 19:32:42 +0100

    Created login request and connected it with frontend application

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/users")
 public class AppUserController {
 
@@ -22,10 +23,29 @@ public class AppUserController {
     private AppUserRepository userRepository;
 
 
-    @PostMapping(value = "/login", produces = "application/json; charset=utf-8")
-    public ResponseEntity<?> loginRequest(@RequestBody String id){
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity loginRequest(@RequestBody String json) throws IOException{
 
-        return new ResponseEntity<>(id, HttpStatus.OK);
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode node = mapper.readTree(json);
+        AppUser appUser = null;
+        appUser = userRepository.findByEmail(node.get("username").asText());
+        System.out.println(node.get("username").asText());
+        if (appUser == null){
+            //return "Invalid email or password";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        if (appUser.getPassword().equals(node.get("password").asText())){
+            //return "Login successsful";
+            return ResponseEntity.ok("test");
+        }
+        else{
+            //return "Invalid email or password";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
