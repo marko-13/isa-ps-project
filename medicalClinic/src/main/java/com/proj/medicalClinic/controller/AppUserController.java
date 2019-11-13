4686858d9ccd081799d2110d4f00323dc2aa8dbe
bokykisac <dervy97@gmail.com>
2019-11-13 21:07:49 +0100

    Adding import for List and Array in class AppUserController

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
@@ -13,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
