c47f99dd391d80c412aa2d2232af60fd09407d31
marko-13 <50908634+marko-13@users.noreply.github.com>
2019-11-17 16:35:15 +0100

    adding authority class

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/AppUser.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/AppUser.java
@@ -1,10 +1,15 @@
 package com.proj.medicalClinic.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -13,7 +18,7 @@ import java.util.UUID;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
-public class AppUser {
+public class AppUser implements UserDetails {
 
 	@Id
 	@GeneratedValue(generator = "UUID")
@@ -40,4 +45,42 @@ public class AppUser {
 	@Column(name="last_name", unique=false, nullable=false)
 	private String lastName;
 
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_authority",
+			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+	private List<Authority> authorities;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@JsonIgnore
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@JsonIgnore
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@JsonIgnore
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
---
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/Authority.java
@@ -0,0 +1,51 @@
+package com.proj.medicalClinic.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name="AUTHORITY")
+public class Authority implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", unique = true, updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name="name")
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    public String getName() {
+        return name;
+    }
+
+    @JsonIgnore
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+}
