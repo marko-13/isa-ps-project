0b06e519d3bbb0f3bc0977ccc55504a65076d470
kresovicmilan <kresovich.milan@gmail.com>
2019-11-19 20:43:40 +0100

    Changing return statement in getAuthorities

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/AppUser.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/AppUser.java
@@ -59,7 +59,7 @@ public class AppUser implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return this.authorities;
 	}
 
 	@Override
