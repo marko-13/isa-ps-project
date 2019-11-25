520505ce91e5d1af1044930404de9aa152114064
bokykisac <dervy97@gmail.com>
2019-11-25 15:42:39 +0100

    Adding medical clinic logo

---
+++ medical-clinic-front/src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import mcLogo from '../../assets/images/MC_logo.png';
+import classes from './Logo.module.css';
+
+const Logo = (props) => {
+    return (
+        <div className={classes.Logo}>
+            <img src={mcLogo} alt='MCLogo'/>
+        </div>
+    );
+};
+
+export default Logo;
