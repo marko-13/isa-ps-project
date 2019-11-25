705cb9094fe4630868a1a71f30997a91b063587f
bokykisac <dervy97@gmail.com>
2019-11-26 00:35:33 +0100

    Correcting text, renaming variables and adding cases for all users

--- medical-clinic-front/src/components/Homepage/UserInfo/UserInfo.js
+++ medical-clinic-front/src/components/Homepage/UserInfo/UserInfo.js
@@ -3,41 +3,53 @@ import React from 'react';
 import doctorImage from '../../../assets/images/doctor.png';
 import patientImage from '../../../assets/images/patient.png';
 import nurseImage from '../../../assets/images/nurse.png';
+import clinicalAdminImage from '../../../assets/images/clinicaladmin.png';
+import clinicalCenterAdminImage from '../../../assets/images/clinicalcenteradmin.png';
 import Button from '../../UI/Button/Button';
 import classes from './UserInfo.module.css';
 import './UserInfo.css';
 
 const UserInfo = (props) => {
     let image
-    let userInfoFields
-    let userInfoFields2 = ''
-    let userInfoFields3 = ''
+
+    let shiftInfo = null;
+    let reviewInfo = null;
+    let jmbgInfo = null;
+    let clinicInfo = null;
+    let clinicalCenterInfo = null;
+
     if(props.role === 'patient'){
       image = patientImage;
-      userInfoFields = (  <tr><td>JMBG:</td><td>12345</td></tr>)
+      jmbgInfo = (  <tr><td>JMBG:</td><td>12345</td></tr>);
     }
     else if (props.role === 'doctor') {
-      image = doctorImage
-      userInfoFields = (<tr><td>Clinic:</td><td>CCCC</td></tr>)
-      userInfoFields2 = (<tr><td>Shift</td><td>DDDD</td></tr>)
-      userInfoFields3 = (<tr><td>Review</td><td>EEEE</td></tr>)
+      image = doctorImage;
+      shiftInfo = (<tr><td>Shift:</td><td>DDDD</td></tr>);
+      reviewInfo = (<tr><td>Review:</td><td>EEEE</td></tr>);
     }
-    else if (props.role == 'nurse') {
-      image = nurseImage
-      userInfoFields = (<tr><td>Clinic:</td><td>CCCC</td></tr>)
-      userInfoFields2 = (<tr><td>Shift</td><td>DDDD</td></tr>)
-      userInfoFields3 = (<tr><td>Review</td><td>EEEE</td></tr>)
+    else if (props.role === 'nurse'){
+      image = nurseImage;
+      shiftInfo = (<tr><td>Shift:</td><td>DDDD</td></tr>);
+    }
+    else if (props.role === 'adminclinic'){
+        image = clinicalAdminImage;
+        clinicInfo = (<tr><td>Clinic:</td><td>CCCC</td></tr>);
+    }
+    
+    else if (props.role === 'adminclinicalcenter'){
+        image = clinicalCenterAdminImage;
+        clinicalCenterInfo = (<tr><td>Clinical Center:</td><td>FFFF</td></tr>);
     }
 
     return (
-                <div class={classes.UserInfo + " col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad"} >
+                <div class={[classes.UserInfo, classes.Table].join(' ') + " col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad"} >
                     <div class="panel panel-info">
                         <div class="panel-heading">
                             <h4 class="panel-title">User profile</h4>
                         </div>
                         <div class="panel-body">
                             <div class="row">
-                                <div class="col-md-4 col-lg-2" align='center'>
+                                <div class="col-md-4 col-lg-2" align='center' style={{marginTop: '20px'}}>
                                     <img alt="User Pic" src={image} class="img-circle img-responsive"/>
                                 </div>
                                 <div class=" col-md-9 col-lg-9 ">
@@ -55,17 +67,19 @@ const UserInfo = (props) => {
                                                 <td>Lastname:</td>
                                                 <td>{props.lastname}</td>
                                             </tr>
-                                            {userInfoFields}
-                                            {userInfoFields2}
-                                            {userInfoFields3}
+                                            {shiftInfo}
+                                            {reviewInfo}
+                                            {jmbgInfo}
+                                            {clinicInfo}
+                                            {clinicalCenterInfo}
                                             <tr>
-                                                <td>Email</td>
+                                                <td>Email:</td>
                                                 <td>info@support.com</td>
                                             </tr>
                                         </tbody>
                                     </table>
-                                    <Button>Edit profile</Button>
-                                    <Button>Change password</Button>
+                                    <Button style={{margin: '0px 5px'}}>Edit profile</Button>
+                                    <Button style={{margin: '0px 5px'}}>Change password</Button>
                                 </div>
                             </div>
                         </div>
