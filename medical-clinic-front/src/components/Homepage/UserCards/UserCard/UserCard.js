fd59af64717b0b7a181a47338ccbac81b8fc97c0
bokykisac <dervy97@gmail.com>
2019-11-26 00:34:11 +0100

    Updating card style and prop names

--- medical-clinic-front/src/components/Homepage/UserCards/UserCard/UserCard.js
+++ medical-clinic-front/src/components/Homepage/UserCards/UserCard/UserCard.js
@@ -6,11 +6,11 @@ import Button from '../../../UI/Button/Button';
 
 const UserCard = (props) => {
     return (
-            <div className={classes.CardItem + " col-sm-4 text-center"} style={{minWidth: '20rem'}}>
+            <div className={classes.CardItem + " col-sm-4 text-center"}>
                 <div className="card">
                     <div className="card-body">
-                        <p className="card-text">{props.tx}</p>
-                        <Button type='grey'>{props.but}</Button>
+                        <p className="card-text">{props.cardText}</p>
+                        <Button type='grey'>{props.buttonText}</Button>
                     </div>
                 </div>
             </div>
