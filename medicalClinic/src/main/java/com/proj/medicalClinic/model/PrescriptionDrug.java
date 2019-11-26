74f1309f23ad3d59a3f2f9d25b5c611d7436054a
kresovicmilan <kresovich.milan@gmail.com>
2019-11-26 14:02:38 +0100

    Adding new class in model that will contain list of all drugs

---
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/PrescriptionDrug.java
@@ -0,0 +1,35 @@
+package com.proj.medicalClinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PrescriptionDrugs {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", unique = true, updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "drug", unique = false, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DrugsType drug;
+
+    private List<Prescription> prescription;
+
+
+}
