c09e864ab45216b3e2c3253838ded80786a2714a
marko-13 <marko07031997@gmail.com>
2019-11-28 00:12:56 +0100

    Create clinicService interface and declare getAllClinis method

---
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/ClinicService.java
@@ -0,0 +1,12 @@
+package com.proj.medicalClinic.service;
+
+import com.proj.medicalClinic.dto.ClinicDTO;
+
+import java.util.List;
+
+public interface ClinicService {
+
+    List<ClinicDTO> getAllClinics ();
+    //List<ClincDTO> findAll();
+
+}
