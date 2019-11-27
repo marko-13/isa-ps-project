c9fb3d2cd6800ed87461f3ec9a93b044230b89b5
kresovicmilan <kresovich.milan@gmail.com>
2019-11-27 18:12:40 +0100

    Updating ids in ResourceConflictException

--- medicalClinic/src/main/java/com/proj/medicalClinic/exception/ResourceConflictException.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/exception/ResourceConflictException.java
@@ -5,18 +5,18 @@ import java.util.UUID;
 public class ResourceConflictException extends RuntimeException {
     private static final long serialVersionUID = 1791564636123821405L;
 
-    private UUID resourceId;
+    private Long resourceId;
 
-    public ResourceConflictException(UUID resourceId, String message) {
+    public ResourceConflictException(Long resourceId, String message) {
         super(message);
         this.setResourceId(resourceId);
     }
 
-    public UUID getResourceId() {
+    public Long getResourceId() {
         return resourceId;
     }
 
-    public void setResourceId(UUID resourceId) {
+    public void setResourceId(Long resourceId) {
         this.resourceId = resourceId;
     }
 
