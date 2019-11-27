fa6c8dd69df929ea9d7b67a2cbe1ff6e780fd6a2
kresovicmilan <kresovich.milan@gmail.com>
2019-11-27 18:17:53 +0100

    Deleting and adding needed imports

--- medicalClinic/src/main/java/com/proj/medicalClinic/model/Prescription.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/model/Prescription.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.*;
 
 @Data
 @Builder
