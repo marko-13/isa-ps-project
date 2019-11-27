cd66a14b5f15406bf248c32b7fe2941c0920e7d9
marko-13 <marko07031997@gmail.com>
2019-11-28 00:10:52 +0100

    Create clinicRepository and add findAll method

---
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/ClinicRepository.java
@@ -0,0 +1,11 @@
+package com.proj.medicalClinic.repository;
+
+import com.proj.medicalClinic.model.Clinic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClinicRepository extends JpaRepository<Clinic, Long> {
+
+    List<Clinic> findAll();
+}
