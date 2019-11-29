973194b09b5796f7158aeb030d600c435844fa4f
bokykisac <dervy97@gmail.com>
2019-11-29 13:53:00 +0100

    Removing old register and login method in AppUserController and chaning route to /user

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
@@ -20,59 +20,11 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/userss")
+@RequestMapping("/user")
 public class AppUserController {
 
     @Autowired
     private AppUserRepository userRepository;
-
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity loginRequest(@RequestBody String json) throws IOException{
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        JsonNode node = mapper.readTree(json);
-        AppUser appUser = null;
-        appUser = userService.findByEmail(node.get("username").asText());
-        System.out.println(node.get("username").asText());
-        if (appUser == null){
-            //return "Invalid email or password";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        if (appUser.getPassword().equals(node.get("password").asText())){
-            //return "Login successsful";
-            return ResponseEntity.ok("test");
-        }
-        else{
-            //return "Invalid email or password";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-    }
-
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity registerRequest(@RequestBody String json) throws IOException {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        JsonNode node = mapper.readTree(json);
-        AppUser appUser = null;
-        appUser = userService.findByEmail(node.get("email").asText());
-
-        if (appUser !=null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists");
-        }
-        else{
-            String name = node.get("name").asText();
-            String password = node.get("password").asText();
-            String email = node.get("email").asText();
-
-            return ResponseEntity.ok("Success");
-        }
-    }
- 
 
     @Autowired
     private AppUserService userService;
