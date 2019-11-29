593a5b570a84e508dea42f1d528ba5fe3287e5bf
bokykisac <dervy97@gmail.com>
2019-11-29 17:17:41 +0100

    Getting info into My profile component  from bekend

--- medical-clinic-front/src/containers/MyProfile/MyProfile.js
+++ medical-clinic-front/src/containers/MyProfile/MyProfile.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import jwt from 'jsonwebtoken';
+import axios from '../../axios';
 
 import Layout from '../../hoc/Layout/Layout';
 import UserDetailsCard from '../../components/UserDetailsCard/UserDetailsCard';
@@ -7,17 +9,64 @@ import UserDetailsCard from '../../components/UserDetailsCard/UserDetailsCard';
 class MyProfile extends Component {
 
     state = {
-
+        isAuth: false,
+        user: {
+            role: '',
+            name: '',
+            lastname: '',
+            email: '',
+            jmbg: '',
+            review: '',
+            shift: '',
+            clinic: '',
+            clinicalCenterId: ''
+        }
     }
 
-    componendDidMount() {
+    componentDidMount() {
 
+        if (localStorage.getItem('token') !== null) {
+            const token = localStorage.getItem('token');
+            const decodedToken = jwt.decode(token);
+            const role = decodedToken.role.toLowerCase();
+
+            axios.get('/user/profile', {
+                headers: { 'Authorization': 'Bearer ' + token }
+            })
+                .then(user => {
+
+                    const updatedUser = {
+                        ...this.state.user
+                    };
+
+                    updatedUser.role = role;
+                    updatedUser.name = user.data.name;
+                    updatedUser.lastname = user.data.lastname;
+                    updatedUser.email = user.data.email
+                    updatedUser.jmbg = user.data.jmbg;
+                    updatedUser.review = user.data.review;
+                    updatedUser.shift = user.data.shift;
+                    updatedUser.clinic = user.data.clinic;
+                    updatedUser.clinicalCenterId = user.data.clinicalCenterId;
+
+                    this.setState({user: updatedUser, isAuth: true})
+                })
+                .catch(err => {
+                    this.setState({isAuth: false});
+                    console.log(err);
+                })
+        } else {
+            this.setState({isAuth: false});
+            console.log('NEMA TOKENA!');
+        }
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <Layout>
-                <UserDetailsCard/>
+                <UserDetailsCard />
             </Layout>
         );
     }
