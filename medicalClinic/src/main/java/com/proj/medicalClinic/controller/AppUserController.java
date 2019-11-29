4104189a22c82d59c2ef1b2a2c34c5c8c14736eb
bokykisac <dervy97@gmail.com>
2019-11-29 14:27:36 +0100

    Adding method getUserProfile to AppUserController

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/AppUserController.java
@@ -2,17 +2,18 @@ package com.proj.medicalClinic.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.proj.medicalClinic.dto.AppUserDTO;
-import com.proj.medicalClinic.model.AppUser;
-import com.proj.medicalClinic.model.RoleType;
+import com.proj.medicalClinic.dto.*;
+import com.proj.medicalClinic.model.*;
+import com.proj.medicalClinic.security.TokenUtils;
 import com.proj.medicalClinic.service.AppUserService;
-import com.proj.medicalClinic.model.Patient;
 import com.proj.medicalClinic.repository.AppUserRepository;
+import com.proj.medicalClinic.service.implementation.CustomUserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,47 @@ public class AppUserController {
 
     @Autowired
     private AppUserService userService;
+
+    @Autowired
+    private CustomUserDetailsServiceImpl customUserDetailsService;
+
+    @Autowired
+    TokenUtils tokenUtils;
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public ResponseEntity<?> getUserProfile() {
+        String email = this.tokenUtils.getUsernameFromToken(this.tokenUtils.getToken(this.httpServletRequest));
+        AppUser appUser = (AppUser) customUserDetailsService.loadUserByUsername(email);
+        String userRole = appUser.getUserRole().name();
+
+        if (userRole.equals("PATIENT")){
+            Patient p = (Patient) appUser;
+            return new ResponseEntity<>(new PatientDTO(p), HttpStatus.OK);
+        }
+        else if (userRole.equals("DOCTOR")){
+            Doctor d = (Doctor) appUser;
+            return new ResponseEntity<>(new DoctorDTO(d), HttpStatus.OK);
+
+        }else if (userRole.equals("NURSE")){
+            Nurse n = (Nurse) appUser;
+            return new ResponseEntity<>(new NurseDTO(n), HttpStatus.OK);
+
+        }else if (userRole.equals("ADMINCLINIC")){
+            AdminClinic ac = (AdminClinic) appUser;
+            return new ResponseEntity<>(new AdminClinicDTO(ac), HttpStatus.OK);
+        }
+        else if (userRole.equals("ADMINCLINICALCENTER")){
+            AdminClinicalCenter acc = (AdminClinicalCenter) appUser;
+            return new ResponseEntity<>(new AdminClinicCenterDTO(acc), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+    }
 
     @GetMapping(value = "/all")
     public ResponseEntity<List<AppUserDTO>> getAllUsers() {
