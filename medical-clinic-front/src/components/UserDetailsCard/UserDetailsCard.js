34dac20c4dd2a9f8de36e0c0e17c7e21c442aac9
bokykisac <dervy97@gmail.com>
2019-11-30 23:32:53 +0100

    Adding generic way to display user informations based on user role

--- medical-clinic-front/src/components/UserDetailsCard/UserDetailsCard.js
+++ medical-clinic-front/src/components/UserDetailsCard/UserDetailsCard.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import Button from '../UI/Button/Button';
+import doctorImage from '../../assets/images/doctor.png';
+import patientImage from '../../assets/images/patient.png';
+import nurseImage from '../../assets/images/nurse.png';
+import clinicalAdminImage from '../../assets/images/clinicaladmin.png';
+import clinicalCenterAdminImage from '../../assets/images/clinicalcenteradmin.png';
 
 import classes from './UserDetailsCard.module.css';
 
-const UserDetailsCard = () => {
+const UserDetailsCard = (props) => {
+
+    let userFields = null;
+
+    userFields = Object.keys(props.user).map(key => {
+        if (props.user[key] !== undefined) {
+            return (
+                <tr key={key}>
+                    <td className={classes.Capitalize}>{key}:</td>
+                    <td>{props.user[key]}</td>
+                </tr>
+            );
+        }
+    });
+
+    let img = null;
+
+    switch(props.user.role){
+        case 'patient': img = patientImage;
+        break;
+        case 'doctor': img = doctorImage;
+        break;
+        case 'nurse': img = nurseImage;
+        break;
+        case 'adminclinic': img = clinicalAdminImage;
+        break;
+        case 'adminclinicalcenter': img = clinicalCenterAdminImage;
+        break;
+
+        default:
+    }
+
     return (
         <div className='container'>
             <div className='row' style={{ margin: '0 5px' }}>
@@ -15,27 +51,16 @@ const UserDetailsCard = () => {
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-md-4 col-lg-2" align='center' style={{ marginTop: '20px' }}>
-                                    <img alt="User Pic" src={''} class="img-circle img-responsive" />
+                                    <img alt="User Pic" src={img} class="img-circle img-responsive" />
                                 </div>
                                 <div class=" col-md-9 col-lg-9 ">
                                     <table class="table table-user-information">
                                         <tbody>
-                                            <tr>
-                                                <td>Role:</td>
-                                                <td></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Name:</td>
-                                                <td></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Lastname:</td>
-                                                <td></td>
-                                            </tr>
+                                            {userFields}
                                         </tbody>
                                     </table>
-                                    <Button style={{margin: '0px 5px'}}>Edit account</Button>
-                                    <Button style={{margin: '0px 5px'}}>Change password</Button>
+                                    <Button style={{ margin: '0px 5px' }}>Edit account</Button>
+                                    <Button style={{ margin: '0px 5px' }}>Change password</Button>
                                 </div>
                             </div>
                         </div>
