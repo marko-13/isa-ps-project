c3010723a3dead7af5954efefc4f87cadb67e24e
marko-13 <50908634+marko-13@users.noreply.github.com>
2019-12-03 18:36:49 +0100

    Logging out user when email is changed Deleting token when email is changed Redirecting user to login register page when email is changed

--- medical-clinic-front/src/containers/MyProfile/UserDataForm/UserDataForm.js
+++ medical-clinic-front/src/containers/MyProfile/UserDataForm/UserDataForm.js
@@ -3,11 +3,14 @@ import axios from '../../../axios';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './UserDataForm.module.css';
+import {withRouter} from 'react-router-dom';
+
 
 class UserDataForm extends Component {
 
     state = {
-        ...this.props.user
+        ...this.props.user,
+        changedEmail: false
     }
 
     onConfirmHandler = (event) => {
@@ -26,13 +29,18 @@ class UserDataForm extends Component {
         })
         .then(response => {
             alert('Success!');
+            if(this.state.changedEmail){
+              this.props.history.push('/');
+              localStorage.removeItem('token');
+            }
+            //alert(localStorage.getItem('token'));
             this.setState({
                 name: '',
                 lastName: '',
                 email: ''
             });
             this.props.closeModal();
-            window.location.reload(false); 
+            window.location.reload(false);
         })
         .catch(err => console.log(err));
     }
@@ -51,15 +59,17 @@ class UserDataForm extends Component {
                 <form>
                     <input type='text' placeholder='Name' className={classes.InputElement} value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })} />
                     <input type='text' placeholder='Lastname' className={classes.InputElement} value={this.state.lastname} onChange={(event) => this.setState({ lastname: event.target.value })} />
-                    <input type='email' placeholder='Email' className={classes.InputElement} value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
+                    <input type='email' placeholder='Email' className={classes.InputElement} value={this.state.email} onChange={(event) => this.setState({ email: event.target.value, changedEmail: true })} />
                     <div style={{float: 'right'}}>
                         <Button style={{ margin: '0px 5px' }} type='green' click={this.onCloseHandler}>Close</Button>
                         <Button style={{ margin: '0px 5px' }} type='green' click={this.onConfirmHandler}>Confirm</Button>
                     </div>
                 </form>
+                <p>* On email change you will be redirected to login/registration page</p>
+
             </div>
         );
     }
 }
 
-export default UserDataForm;
+export default withRouter(UserDataForm);
