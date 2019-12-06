ac26524b236b9be64e5744d2372799d1f68a0146
bokykisac <dervy97@gmail.com>
2019-12-06 17:16:39 +0100

    Adding additional fields in NurseDTO

--- medicalClinic/src/main/java/com/proj/medicalClinic/dto/NurseDTO.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/dto/NurseDTO.java
@@ -16,12 +16,20 @@ public class NurseDTO {
     private String lastname;
     private String email;
     private int shift;
+    private String address;
+    private String city;
+    private String state;
+    private String mobile;
 
     public NurseDTO (Nurse n){
         this.name = n.getName();
         this.lastname = n.getLastName();
         this.email = n.getEmail();
         this.shift = n.getShift();
+        this.address = n.getAdress();
+        this.city = n.getCity();
+        this.state = n.getState();
+        this.mobile = n.getMobile();
     }
 
 }
