7a2b0634bbb06446272751e5918b9fe43013f1fe
bokykisac <dervy97@gmail.com>
2019-12-06 17:16:07 +0100

    Adding additional fields in AdminClinicCenterDTO

--- medicalClinic/src/main/java/com/proj/medicalClinic/dto/AdminClinicCenterDTO.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/dto/AdminClinicCenterDTO.java
@@ -16,12 +16,20 @@ public class AdminClinicCenterDTO {
     private String lastname;
     private String email;
     private Long clinicalCenterId;
+    private String address;
+    private String city;
+    private String state;
+    private String mobile;
 
     public AdminClinicCenterDTO(AdminClinicalCenter acc){
         this.name = acc.getName();
         this.lastname = acc.getLastName();
         this.email = acc.getEmail();
         this.clinicalCenterId = acc.getClinicalCenter().getId();
+        this.address = acc.getAdress();
+        this.city = acc.getCity();
+        this.state = acc.getState();
+        this.mobile = acc.getMobile();
     }
 
 }
