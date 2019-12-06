86d2ce70e6b1bdc7ed12e60a9897d5ec8353e780
bokykisac <dervy97@gmail.com>
2019-12-06 17:21:30 +0100

    Adding additional information  to user request and adding additional input fields

--- medical-clinic-front/src/containers/MyProfile/UserDataForm/UserDataForm.js
+++ medical-clinic-front/src/containers/MyProfile/UserDataForm/UserDataForm.js
@@ -19,10 +19,12 @@ class UserDataForm extends Component {
         const updatedUser = {
                             name: this.state.name,
                             lastName: this.state.lastname,
-                            email: this.state.email
+                            email: this.state.email,
+                            adress: this.state.address,
+                            city: this.state.city,
+                            state: this.state.state,
+                            mobile: this.state.mobile
                         };
-
-        console.log(updatedUser);
 
         axios.post('/user/update', updatedUser, {
             headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') }
@@ -37,7 +39,11 @@ class UserDataForm extends Component {
             this.setState({
                 name: '',
                 lastName: '',
-                email: ''
+                email: '',
+                address: '',
+                city: '',
+                state: '',
+                mobile: ''
             });
             this.props.closeModal();
             window.location.reload(false);
@@ -60,6 +66,10 @@ class UserDataForm extends Component {
                     <input type='text' placeholder='Name' className={classes.InputElement} value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })} />
                     <input type='text' placeholder='Lastname' className={classes.InputElement} value={this.state.lastname} onChange={(event) => this.setState({ lastname: event.target.value })} />
                     <input type='email' placeholder='Email' className={classes.InputElement} value={this.state.email} onChange={(event) => this.setState({ email: event.target.value, changedEmail: true })} />
+                    <input type='text' placeholder='Address' className={classes.InputElement} value={this.state.address} onChange={(event) => this.setState({ address: event.target.value })} />
+                    <input type='text' placeholder='City' className={classes.InputElement} value={this.state.city} onChange={(event) => this.setState({ city: event.target.value })} />
+                    <input type='text' placeholder='State' className={classes.InputElement} value={this.state.state} onChange={(event) => this.setState({ state: event.target.value })} />
+                    <input type='text' placeholder='Mobile' className={classes.InputElement} value={this.state.mobile} onChange={(event) => this.setState({ mobile: event.target.value })} />
                     <div style={{float: 'right'}}>
                         <Button style={{ margin: '0px 5px' }} type='green' click={this.onCloseHandler}>Close</Button>
                         <Button style={{ margin: '0px 5px' }} type='green' click={this.onConfirmHandler}>Confirm</Button>
