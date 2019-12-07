e8b560fdd8ab9797d5b711a89e474f56a71d239a
bokykisac <dervy97@gmail.com>
2019-12-07 18:32:54 +0100

    Adding component for dispaying operation rooms

---
+++ medical-clinic-front/src/containers/OperationRooms/OperationRooms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OperationRooms extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        );
+    }
+}
+
+export default OperationRooms;
