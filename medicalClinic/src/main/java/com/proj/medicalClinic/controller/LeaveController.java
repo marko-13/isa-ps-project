1d62788b8c44b6d7cf7137abade684242a0a452b
bokykisac <dervy97@gmail.com>
2019-12-10 22:14:46 +0100

    Adding deny and approve endpoints in leave controller

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/LeaveController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/LeaveController.java
@@ -2,15 +2,14 @@ package com.proj.medicalClinic.controller;
 
 
 import com.proj.medicalClinic.dto.LeaveDTO;
+import com.proj.medicalClinic.model.AppUser;
 import com.proj.medicalClinic.service.LeaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
 import java.util.List;
@@ -27,6 +26,28 @@ public class LeaveController {
     public ResponseEntity<?> getAllLeaves(){
         List<LeaveDTO> leaveDTOS = leaveService.getAll();
         return new ResponseEntity<>(leaveDTOS, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/approveLeave/{id}", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMINCLINIC')")
+    public ResponseEntity<?> approveLeave(@PathVariable Long id, @RequestBody String email){
+        leaveService.approveLeave(id,email);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/denyLeave/{id}", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMINCLINIC')")
+    public ResponseEntity<?> denyLeave(@PathVariable Long id, @RequestBody DeniedLeave deniedLeave){
+        String email = deniedLeave.email;
+        String message = deniedLeave.message;
+        leaveService.denyLeave(id, email, " your request for leave of absence has been denied. \n\nReason:\n" + message);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+    static class DeniedLeave {
+        public String email;
+        public String message;
+
     }
 
 }
