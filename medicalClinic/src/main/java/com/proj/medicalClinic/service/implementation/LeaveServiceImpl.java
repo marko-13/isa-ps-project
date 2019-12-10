51eda9d57a56c679c41e1daa9c4b8414faef145a
bokykisac <dervy97@gmail.com>
2019-12-10 18:35:31 +0100

    Updating leave service to return all information about user

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/LeaveServiceImpl.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/LeaveServiceImpl.java
@@ -2,9 +2,12 @@ package com.proj.medicalClinic.service.implementation;
 
 import com.proj.medicalClinic.dto.DoctorDTO;
 import com.proj.medicalClinic.dto.LeaveDTO;
+import com.proj.medicalClinic.dto.NurseDTO;
 import com.proj.medicalClinic.exception.NotExistsException;
 import com.proj.medicalClinic.model.AppUser;
+import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.model.Leave;
+import com.proj.medicalClinic.model.Nurse;
 import com.proj.medicalClinic.repository.AppUserRepository;
 import com.proj.medicalClinic.repository.LeaveRepository;
 import com.proj.medicalClinic.service.LeaveService;
@@ -12,6 +15,7 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -36,7 +40,15 @@ public class LeaveServiceImpl implements LeaveService {
         List<LeaveDTO> leaveDTOS = new ArrayList<>();
 
         for(Leave l : leaves){
-            leaveDTOS.add(new LeaveDTO(l));
+            if(l.getNurse() != null){
+                AppUser appUser = appUserRepository.findById(l.getNurse().getId()).orElseThrow(NotExistsException::new);
+                NurseDTO nurseDTO = new NurseDTO((Nurse) appUser);
+                leaveDTOS.add(new LeaveDTO(l, nurseDTO));
+            }else{
+                AppUser appUser = appUserRepository.findById(l.getDoctor().getId()).orElseThrow(NotExistsException::new);
+                DoctorDTO doctorDTO = new DoctorDTO((Doctor) appUser);
+                leaveDTOS.add(new LeaveDTO(l, doctorDTO));
+            }
         }
 
         return leaveDTOS;
