b1ad2f916b684f3835ae90a5a4a1b483b2d788e2
marko-13 <marko07031997@gmail.com>
2019-12-11 23:57:37 +0100

    Renaming getAll to get, as suggested

--- medical-clinic-front/src/components/Clinic/ShowClinics/ShowClinics.js
+++ medical-clinic-front/src/components/Clinic/ShowClinics/ShowClinics.js
@@ -11,7 +11,7 @@ class ShowClinics extends Component{
 
   componentDidMount(){
     const token = localStorage.getItem('token');
-    axios.get('/clinics/getAll', {
+    axios.get('/clinics/get', {
       headers: {'Authorization': 'Bearer ' + token}
     } )
       .then(res => {
