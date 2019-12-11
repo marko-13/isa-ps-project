e145401f12f30a723ec23a5567136e3bdaf9482b
bokykisac <dervy97@gmail.com>
2019-12-11 22:05:46 +0100

    Creating test for leave repository

---
+++ medicalClinic/src/test/java/com/proj/medicalClinic/TestSuite.java
@@ -0,0 +1,11 @@
+package com.proj.medicalClinic;
+
+import com.proj.medicalClinic.repository.LeaveRepositoryTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({LeaveRepositoryTest.class,})
+public class TestSuite {
+}
+
