23c0c67760dcdd19fe13c329d2f0fc877c98875a
marko-13 <50908634+marko-13@users.noreply.github.com>
2019-12-11 13:53:38 +0100

    Adding getAllByPatient method

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/AppointmentService.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/AppointmentService.java
@@ -1,10 +1,12 @@
 package com.proj.medicalClinic.service;
 
 import com.proj.medicalClinic.dto.AppointmentDTO;
+import com.proj.medicalClinic.dto.AppointmentHistoryDTO;
 
 import java.util.List;
 
 public interface AppointmentService {
 
     List<AppointmentDTO> getAllByOperationRoom(Long id);
+    List<AppointmentHistoryDTO> getAllByPatient(Long id);
 }
