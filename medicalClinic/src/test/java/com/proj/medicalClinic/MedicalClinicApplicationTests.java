c976b252d743c9fc310870a383513e3f7f227f68
marko-13 <marko07031997@gmail.com>
2019-12-12 18:24:35 +0100

    Changing test import

--- medicalClinic/src/test/java/com/proj/medicalClinic/MedicalClinicApplicationTests.java
+++ medicalClinic/src/test/java/com/proj/medicalClinic/MedicalClinicApplicationTests.java
@@ -1,7 +1,9 @@
 package com.proj.medicalClinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
