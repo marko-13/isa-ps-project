7b22ffea16f4c01c315158dcac12b3edc878fa94
marko-13 <50908634+marko-13@users.noreply.github.com>
2019-12-18 00:09:09 +0100

    Import AppUser class to Appointement repository

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/AppointmentRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package com.proj.medicalClinic.repository;
 
+import com.proj.medicalClinic.model.AppUser;
 import com.proj.medicalClinic.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
