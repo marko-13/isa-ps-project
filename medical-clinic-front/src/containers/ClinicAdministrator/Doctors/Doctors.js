31fa2f5ffedc4fdbf7e5bc62f3a47cfc6ab31ec5
bokykisac <dervy97@gmail.com>
2019-12-27 14:59:54 +0100

    Adding container for showing list of all doctors and form for adding new doctor

---
+++ medical-clinic-front/src/containers/ClinicAdministrator/Doctors/Doctors.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import ReactTable from 'react-table-6';
+
+import Button from '../../../components/UI/Button/Button';
+import NewDoctor from '../../../components/Forms/NewDoctor/NewDoctor';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+
+class Doctors extends Component {
+
+    state = {
+        doctors: null
+    }
+
+    componentDidMount() {
+        this.getAllDoctors();
+    }
+
+    getAllDoctors() {
+        axios.get('doctor/getAll')
+            .then(response => this.setState({ doctors: response.data }))
+            .catch(err => console.log(err));
+    }
+
+    render() {
+
+        let table = null;
+
+        if (this.state.doctors !== null) {
+            const data = this.state.doctors;
+
+            const columns = [{
+                Header: 'List of doctors',
+                columns: [
+                    {
+                        id: 'name',
+                        Header: 'Name',
+                        accessor: d => d.name
+                    },
+                    {
+                        id: 'lastname',
+                        Header: 'Lastname',
+                        accessor: d => d.lastname
+                    },
+                    {
+                        id: 'email',
+                        Header: 'Email',
+                        accessor: d => d.email
+                    },
+                    {
+                        id: 'shift',
+                        Header: 'Shift',
+                        accessor: d => d.shift
+                    },
+                    {
+                        Header: "",
+                        Cell: ({ original }) => (
+                            <center><Button type='red'>Remove</Button></center>),
+                        filterable: false,
+                        sortable: false
+                    }]
+            }];
+
+            table = (
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    className="-striped"
+                    pageSize={10}
+                    filterable={true}
+                    pageSize={(this.state.doctors.length > 10) ? 10 : this.state.doctors.length}
+                    defaultFilterMethod={(filter, row, column) => {
+                        const id = filter.pivotId || filter.id
+                        return row[id] !== undefined ? String(row[id]).toLowerCase().includes(filter.value.toLowerCase()) : true
+                    }}
+                />
+            );
+        }
+
+        return (
+            <Auxiliary>
+                <div className='col-6'>
+                    {table}
+                </div>
+                <div className='col-6'>
+                    <NewDoctor />
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Doctors;
