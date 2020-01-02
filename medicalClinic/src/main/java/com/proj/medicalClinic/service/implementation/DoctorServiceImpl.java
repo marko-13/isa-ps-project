9e1c9309cefc71dc840ceaa3460413e185ddb41b
bokykisac <dervy97@gmail.com>
2020-01-02 22:11:00 +0100

    Implementing save new doctor in doctor service

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/DoctorServiceImpl.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/DoctorServiceImpl.java
@@ -2,8 +2,11 @@ package com.proj.medicalClinic.service.implementation;
 
 import com.proj.medicalClinic.dto.DoctorDTO;
 import com.proj.medicalClinic.exception.NotExistsException;
+import com.proj.medicalClinic.model.Authority;
 import com.proj.medicalClinic.model.Doctor;
+import com.proj.medicalClinic.model.RoleType;
 import com.proj.medicalClinic.repository.DoctorRepository;
+import com.proj.medicalClinic.service.AuthorityService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,9 @@ public class DoctorServiceImpl {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Autowired
+    private AuthorityService authorityService;
+
     public List<DoctorDTO> getAll(){
         List<Doctor> doctors = doctorRepository.findAll();
         if(doctors == null){
@@ -29,6 +35,29 @@ public class DoctorServiceImpl {
         }
 
         return doctorDTOS;
+    }
+
+    public DoctorDTO save(Doctor doctorRequest) {
+        Doctor doctor = new Doctor();
+
+        doctor.setName(doctorRequest.getName());
+        doctor.setAdress(doctorRequest.getAdress());
+        doctor.setCity(doctorRequest.getCity());
+        doctor.setEmail(doctorRequest.getEmail());
+        doctor.setLastName(doctorRequest.getLastName());
+        doctor.setMobile(doctorRequest.getMobile());
+        doctor.setPassword("12321");
+        doctor.setState(doctorRequest.getState());
+        doctor.setUserRole(RoleType.DOCTOR);
+        doctor.setReview(0);
+        doctor.setReviewCount(0);
+        doctor.setShift(doctorRequest.getShift());
+
+        List<Authority> auth = authorityService.findByName(doctorRequest.getUserRole().name());
+        doctor.setAuthorities(auth);
+        doctorRepository.save(doctor);
+
+        return new DoctorDTO(doctor);
     }
 
 }
