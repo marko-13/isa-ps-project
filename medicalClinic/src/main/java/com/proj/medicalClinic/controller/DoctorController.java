3d02a19dd1b12527b3794f3850f9c8fc62a71bf2
bokykisac <dervy97@gmail.com>
2020-01-02 22:10:38 +0100

    Adding endpoint for saving new doctor in doctor controller

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/DoctorController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/DoctorController.java
@@ -1,11 +1,13 @@
 package com.proj.medicalClinic.controller;
 
 import com.proj.medicalClinic.dto.DoctorDTO;
+import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.service.DoctorService;
 import com.proj.medicalClinic.service.implementation.DoctorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,11 @@ public class DoctorController {
     public ResponseEntity<?> getAllDoctors() {
         List<DoctorDTO> doctorDTOS = doctorService.getAll();
         return new ResponseEntity<>(doctorDTOS, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity<?> saveDoctor(@RequestBody Doctor doctor) {
+        DoctorDTO doctorDTO = doctorService.save(doctor);
+        return new ResponseEntity<>(doctorDTO, HttpStatus.OK);
     }
 }
