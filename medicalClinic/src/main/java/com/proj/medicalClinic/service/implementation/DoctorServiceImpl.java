14a31f03e9862478a0e073b3279aba81a3ea7bfa
bokykisac <dervy97@gmail.com>
2020-01-05 18:05:01 +0100

    Adding and implemeting method for doctor removal

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/DoctorServiceImpl.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/DoctorServiceImpl.java
@@ -2,20 +2,27 @@ package com.proj.medicalClinic.service.implementation;
 
 import com.proj.medicalClinic.dto.DoctorDTO;
 import com.proj.medicalClinic.exception.NotExistsException;
+import com.proj.medicalClinic.exception.NotValidParamsException;
+import com.proj.medicalClinic.exception.ResourceConflictException;
+import com.proj.medicalClinic.model.AppUser;
 import com.proj.medicalClinic.model.Authority;
 import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.model.RoleType;
+import com.proj.medicalClinic.repository.AppUserRepository;
 import com.proj.medicalClinic.repository.DoctorRepository;
+import com.proj.medicalClinic.service.AppUserService;
 import com.proj.medicalClinic.service.AuthorityService;
+import com.proj.medicalClinic.service.DoctorService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
-public class DoctorServiceImpl {
+public class DoctorServiceImpl implements DoctorService {
 
     @Autowired
     private DoctorRepository doctorRepository;
@@ -23,8 +30,11 @@ public class DoctorServiceImpl {
     @Autowired
     private AuthorityService authorityService;
 
+    @Autowired
+    private AppUserRepository userRepository;
+
     public List<DoctorDTO> getAll(){
-        List<Doctor> doctors = doctorRepository.findAll();
+        List<Doctor> doctors = doctorRepository.findAllByDeletedNot(true);
         if(doctors == null){
             throw new NotExistsException();
         }
@@ -52,10 +62,25 @@ public class DoctorServiceImpl {
         doctor.setReview(0);
         doctor.setReviewCount(0);
         doctor.setShift(doctorRequest.getShift());
+        doctor.setDeleted(false);
 
         List<Authority> auth = authorityService.findByName(doctorRequest.getUserRole().name());
         doctor.setAuthorities(auth);
         doctorRepository.save(doctor);
+
+        return new DoctorDTO(doctor);
+    }
+
+    public DoctorDTO remove(Long id){
+
+        Doctor doctor = doctorRepository.findById(id).orElseThrow(NotExistsException::new);
+
+        if(doctor.getExaminations().isEmpty() && doctor.getOperations().isEmpty()){
+            doctor.setDeleted(true);
+            doctorRepository.save(doctor);
+        }else{
+            throw new ResourceConflictException(id, "Doktor ima zakazane preglede!");
+        }
 
         return new DoctorDTO(doctor);
     }
