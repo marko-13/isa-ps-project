62d556202f3dfdc40e23617836168d85fe7390df
bokykisac <dervy97@gmail.com>
2020-01-05 18:04:40 +0100

    Changing findAll to find all doctors that are not deleted and adding native querry for finding doctor by id

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/DoctorRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/DoctorRepository.java
@@ -2,11 +2,19 @@ package com.proj.medicalClinic.repository;
 
 import com.proj.medicalClinic.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
-    List<Doctor> findAll();
+    List<Doctor> findAllByDeletedNot(boolean deleted);
+    
+    @Query(
+            value = "SELECT * FROM app_user d WHERE d.id = ?1",
+            nativeQuery = true)
+    Optional<Doctor> findById(Long id);
 }
 
