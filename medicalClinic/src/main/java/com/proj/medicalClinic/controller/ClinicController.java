f6bb74d63e672dd49225a68350be81d79caaf519
bokykisac <dervy97@gmail.com>
2020-01-06 21:47:44 +0100

    Adding endpoint for getting clinic by administrator clinic ID

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/ClinicController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/ClinicController.java
@@ -1,9 +1,12 @@
 package com.proj.medicalClinic.controller;
 
+import com.proj.medicalClinic.dto.ClinicDTO;
+import com.proj.medicalClinic.exception.NotExistsException;
 import com.proj.medicalClinic.security.TokenUtils;
 import com.proj.medicalClinic.service.AppUserService;
 import com.proj.medicalClinic.service.ClinicService;
 import com.proj.medicalClinic.service.implementation.CustomUserDetailsServiceImpl;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +47,17 @@ public class ClinicController {
     public ResponseEntity<?> getAllClinics() {
 
         return new ResponseEntity<>(this.clinicService.getAllClinics(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getByAdmin/{id}")
+    public ResponseEntity<?> getClinicByAdminId(@PathVariable long id){
+        try{
+            ClinicDTO clinicDTO = clinicService.getClinicByAdmin(id);
+            return new ResponseEntity<>(clinicDTO, HttpStatus.OK);
+        }catch (NotExistsException e){
+            return new ResponseEntity<>("Klinika nije pronadjena!", HttpStatus.NOT_FOUND);
+        }
+
+
     }
 }
