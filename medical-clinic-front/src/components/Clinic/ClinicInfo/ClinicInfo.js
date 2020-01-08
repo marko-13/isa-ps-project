b24f9d7f803faf9db9c65234130a11009f8de75e
bokykisac <dervy97@gmail.com>
2020-01-08 19:50:29 +0100

    Importing modal form for chaning clinic info in clinicInfo component

--- medical-clinic-front/src/components/Clinic/ClinicInfo/ClinicInfo.js
+++ medical-clinic-front/src/components/Clinic/ClinicInfo/ClinicInfo.js
@@ -4,58 +4,82 @@ import axios from '../../../axios';
 
 import classes from './ClinicInfo.module.css';
 import edit from '../../../assets/images/edit.png';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+import Modal from '../../UI/Modal/Modal';
+import EditClinic from '../../Forms/EditClinic/EditClinic';
 
 class ClinicInfo extends Component {
 
     state = {
         error: false,
-        clinic: null
+        clinic: null,
+        modalOpen: false
     }
 
     componentDidMount() {
-        if(localStorage.getItem('token') !== null){
+        if (localStorage.getItem('token') !== null) {
             const token = localStorage.getItem('token');
             const decodedToken = jwt.decode(token);
             const userId = decodedToken.userId;
             console.log(userId);
 
             axios.get('/clinics/getByAdmin/' + userId)
-                .then(clinic => this.setState({clinic: clinic.data}))
+                .then(clinic => this.setState({ clinic: clinic.data }))
                 .catch(err => console.log(err));
 
-        }else{
-            this.setState({error: true});
+        } else {
+            this.setState({ error: true });
         }
+    }
+
+    modalCloseHandler = () => {
+        this.setState({ modalOpen: false });
+    }
+
+    modalOpenHandler = () => {
+        this.setState({ modalOpen: true });
+    }
+
+    updateClinicHandler = (updatedClinic) => {
+
+        const clinic = {
+            ...updatedClinic
+        }
+
+        this.setState({clinic: clinic});
     }
 
     render() {
 
-        console.log(this.state.clinic);
-
         let ret = null;
 
-        if(this.state.clinic === null){
+        if (this.state.clinic === null) {
             ret = <h1>Ucitavanje...</h1>
-            if(this.state.error){
+            if (this.state.error) {
                 ret = <h1>Doslo je do greske</h1>
             }
-        }else{
+        } else {
             ret = (
-                <div>
-                    <div className={classes.Header}>
-                        <h3>Clinic details</h3>
-                    </div>
-                    <img src={edit} className={classes.Edit} />
-                    <div className={classes.Klinika}>
-                        <h4>Name: {this.state.clinic.name}</h4>
-                        <h5>Address: {this.state.clinic.address}</h5>
+                <Auxiliary>
+                    <div>
+                        <div className={classes.Header}>
+                            <h3>Clinic details</h3>
+                        </div>
+                        <img src={edit} className={classes.Edit} onClick={this.modalOpenHandler} />
+                        <div className={classes.Klinika}>
+                            <h4>Name: {this.state.clinic.name}</h4>
+                            <h5>Address: {this.state.clinic.address}</h5>
 
-                        <h6 className={classes.Description}>Description:</h6>
-                        <p style={{paddingLeft: '8px', marginBottom: '25px'}}>{this.state.clinic.description}</p>
+                            <h6 className={classes.Description}>Description:</h6>
+                            <p style={{ paddingLeft: '8px', marginBottom: '25px' }}>{this.state.clinic.description}</p>
 
-                        <h5>Clinic review (0-10): <span className={classes.Review}>{this.state.clinic.review}</span></h5>
+                            <h5>Clinic review (0-10): <span className={classes.Review}>{this.state.clinic.review}</span></h5>
+                        </div>
                     </div>
-                </div>
+                    <Modal show={this.state.modalOpen} modalClosed={this.state.modalCloseHandler}>
+                        <EditClinic clinic={this.state.clinic} closeModal={this.modalCloseHandler} updateClinic={this.updateClinicHandler} />
+                    </Modal>
+                </Auxiliary>
             );
         }
 
