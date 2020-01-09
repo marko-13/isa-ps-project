2780d63b3dbb7b927e9a487866706652dd513efe
kresovicmilan <kresovich.milan@gmail.com>
2020-01-09 19:52:25 +0100

    Check if password has never been changed

--- medical-clinic-front/src/components/Homepage/Homepage.js
+++ medical-clinic-front/src/components/Homepage/Homepage.js
@@ -47,114 +47,119 @@ const Homepage = (props) => {
 
     let functions = null;
 
+    if (passChanged) {
+        switch (role) {
+            case 'nurse':
+                page = (
+                    <Auxiliary>
+                        <UserInfo name={name} lastname={lastname} role={role} />
+                        <UserCards>
+                            <UserCard buttonText={"Work schedule"} cardText={"Shows work schedule"} link = {'/homepage/workschedule'}/>
+                            <UserCard buttonText={"Validate perscriptions"} cardText={"Shows a list of perscriptions that need to be validated"}  link ={'/homepage/nurse/prescriptions'}/>
+                            <UserCard buttonText={"List of patients"} cardText={"Shows a list of patients"}  link ={'/homepage/patient/clinics'}/>
+                            <UserCard buttonText={"Ask for leave of absence"} cardText={"Shows form for leave of absence"}  link ={'/homepage/patient/clinics'}/>
+                        </UserCards>
+                    </Auxiliary>
+                );
+                functions = (
+                  <Auxiliary>
+                    <Route path='/homepage/nurse/prescriptions' component={Prescriptions}></Route>
+                    <Route path='/homepage/patient/medicalstaff' render={null}></Route>
+                    <Route path='/homepage/patient/medicalhistory' component={ShowMedicalHistory}></Route>
+                    <Route path='/homepage/patient/appointments' render={null}></Route>
+                  </Auxiliary>
+                );
+                break;
+            case 'patient':
+                page = (
+                    <Auxiliary>
+                        <UserInfo name={name} lastname={lastname} role={role} />
+                        <UserCards>
+                            <UserCard buttonText={"Inspect clinics"} cardText={"Shows a list of all existing clinics in clinical center"} link ={'/homepage/patient/clinics'}/>
+                            <UserCard buttonText={"Inspect medical staff"} cardText={"Shows a list of every medical staff member, nurses and doctors"} link ={'/homepage/patient/medicalstaff'}/>
+                            <UserCard buttonText={"Inspect medical history"} cardText={"Shows a list of patients medical history"} link={'/homepage/patient/medicalhistory'}/>
+                            <UserCard buttonText={"Inspect appointments"} cardText={"Shows a list of patients upcoming and past appointments"} link={'/homepage/patient/appointments'}/>
+                        </UserCards>
+                    </Auxiliary>
 
-    switch (role) {
-        case 'nurse':
-            page = (
-                <Auxiliary>
-                    <UserInfo name={name} lastname={lastname} role={role} />
-                    <UserCards>
-                        <UserCard buttonText={"Work schedule"} cardText={"Shows work schedule"} link = {'/homepage/workschedule'}/>
-                        <UserCard buttonText={"Validate perscriptions"} cardText={"Shows a list of perscriptions that need to be validated"}  link ={'/homepage/nurse/prescriptions'}/>
-                        <UserCard buttonText={"List of patients"} cardText={"Shows a list of patients"}  link ={'/homepage/patient/clinics'}/>
-                        <UserCard buttonText={"Ask for leave of absence"} cardText={"Shows form for leave of absence"}  link ={'/homepage/patient/clinics'}/>
-                    </UserCards>
-                </Auxiliary>
-            );
-            functions = (
-              <Auxiliary>
-                <Route path='/homepage/nurse/prescriptions' component={Prescriptions}></Route>
-                <Route path='/homepage/patient/medicalstaff' render={null}></Route>
-                <Route path='/homepage/patient/medicalhistory' component={ShowMedicalHistory}></Route>
-                <Route path='/homepage/patient/appointments' render={null}></Route>
-              </Auxiliary>
-            );
-            break;
-        case 'patient':
-            page = (
-                <Auxiliary>
-                    <UserInfo name={name} lastname={lastname} role={role} />
-                    <UserCards>
-                        <UserCard buttonText={"Inspect clinics"} cardText={"Shows a list of all existing clinics in clinical center"} link ={'/homepage/patient/clinics'}/>
-                        <UserCard buttonText={"Inspect medical staff"} cardText={"Shows a list of every medical staff member, nurses and doctors"} link ={'/homepage/patient/medicalstaff'}/>
-                        <UserCard buttonText={"Inspect medical history"} cardText={"Shows a list of patients medical history"} link={'/homepage/patient/medicalhistory'}/>
-                        <UserCard buttonText={"Inspect appointments"} cardText={"Shows a list of patients upcoming and past appointments"} link={'/homepage/patient/appointments'}/>
-                    </UserCards>
-                </Auxiliary>
+                );
+                functions = (
+                  <Auxiliary>
+                    <Route path='/homepage/patient/clinics' component={ShowClinics}></Route>
+                    <Route path='/homepage/patient/medicalstaff' component={ShowMedicalStaff}></Route>
+                    <Route path='/homepage/patient/medicalhistory' component={ShowMedicalHistory}></Route>
+                    <Route path='/homepage/patient/appointments' component={ShowAppointments}></Route>
+                  </Auxiliary>
+                );
+                break;
+            case 'doctor':
+                page = (
+                    <Auxiliary>
+                        <UserInfo name={name} lastname={lastname} role={role} />
+                        <UserCards>
+                            <UserCard buttonText={"List of patients"} cardText={"Shows a list of patients"}  link ={'/homepage/patient/clinics'}/>
+                            <UserCard buttonText={"Start exam"} cardText={"Starta a new medical exam"} link ={'/homepage/patient/clinics'} />
+                            <UserCard buttonText={"Work schedule"} cardText={"Shows my work schedule"}  link ={'/homepage/patient/clinics'}/>
+                            <UserCard buttonText={"Ask for leave of absence"} cardText={"Shows form for leave of absence"}  link ={'/homepage/patient/clinics'}/>
+                            <UserCard buttonText={"Schedule"} cardText={"Schedule an appointment for operation or medical exam"}  link ={'/homepage/patient/clinics'}/>
+                        </UserCards>
+                    </Auxiliary>
 
-            );
-            functions = (
-              <Auxiliary>
-                <Route path='/homepage/patient/clinics' component={ShowClinics}></Route>
-                <Route path='/homepage/patient/medicalstaff' component={ShowMedicalStaff}></Route>
-                <Route path='/homepage/patient/medicalhistory' component={ShowMedicalHistory}></Route>
-                <Route path='/homepage/patient/appointments' component={ShowAppointments}></Route>
-              </Auxiliary>
-            );
-            break;
-        case 'doctor':
-            page = (
-                <Auxiliary>
-                    <UserInfo name={name} lastname={lastname} role={role} />
-                    <UserCards>
-                        <UserCard buttonText={"List of patients"} cardText={"Shows a list of patients"}  link ={'/homepage/patient/clinics'}/>
-                        <UserCard buttonText={"Start exam"} cardText={"Starta a new medical exam"} link ={'/homepage/patient/clinics'} />
-                        <UserCard buttonText={"Work schedule"} cardText={"Shows my work schedule"}  link ={'/homepage/patient/clinics'}/>
-                        <UserCard buttonText={"Ask for leave of absence"} cardText={"Shows form for leave of absence"}  link ={'/homepage/patient/clinics'}/>
-                        <UserCard buttonText={"Schedule"} cardText={"Schedule an appointment for operation or medical exam"}  link ={'/homepage/patient/clinics'}/>
-                    </UserCards>
-                </Auxiliary>
+                );
+                break;
+            case 'adminclinic':
+                page = (
+                    <Auxiliary>
+                        <UserInfo name={name} lastname={lastname} role={role} />
+                        <UserCards>
+                            <UserCard buttonText={"Operation Rooms"} cardText={"Search and filter operation rooms"}  link ={'/homepage/admin-clinic/operation-rooms'}/>
+                            <UserCard buttonText={"Show"} cardText={"Show all requests for leave of absence"}  link ={'/homepage/admin-clinic/leaves'}/>
+                            <UserCard buttonText={"Show doctors"} cardText={"Search, add or remove doctors"}  link ={'/homepage/admin-clinic/doctors'}/>
+                            <UserCard buttonText={"Clinic's profile"} cardText={"Show and modify clinic's profile"}  link ={'/homepage/admin-clinic/clinic'}/>
+                        </UserCards>
+                    </Auxiliary>
 
-            );
-            break;
-        case 'adminclinic':
-            page = (
-                <Auxiliary>
-                    <UserInfo name={name} lastname={lastname} role={role} />
-                    <UserCards>
-                        <UserCard buttonText={"Operation Rooms"} cardText={"Search and filter operation rooms"}  link ={'/homepage/admin-clinic/operation-rooms'}/>
-                        <UserCard buttonText={"Show"} cardText={"Show all requests for leave of absence"}  link ={'/homepage/admin-clinic/leaves'}/>
-                        <UserCard buttonText={"Show doctors"} cardText={"Search, add or remove doctors"}  link ={'/homepage/admin-clinic/doctors'}/>
-                        <UserCard buttonText={"Clinic's profile"} cardText={"Show and modify clinic's profile"}  link ={'/homepage/admin-clinic/clinic'}/>
-                    </UserCards>
-                </Auxiliary>
+                );
 
-            );
+                functions = (
+                    <Auxiliary>
+                      <Route path='/homepage/admin-clinic/operation-rooms' component={OperationRooms}></Route>
+                      <Route path='/homepage/admin-clinic/leaves' component={Leaves}></Route>
+                      <Route path='/homepage/admin-clinic/doctors' component={Doctors}></Route>
+                      <Route path='/homepage/admin-clinic/clinic' component={Clinic}></Route>
+                    </Auxiliary>
+                  );
 
-            functions = (
-                <Auxiliary>
-                  <Route path='/homepage/admin-clinic/operation-rooms' component={OperationRooms}></Route>
-                  <Route path='/homepage/admin-clinic/leaves' component={Leaves}></Route>
-                  <Route path='/homepage/admin-clinic/doctors' component={Doctors}></Route>
-                  <Route path='/homepage/admin-clinic/clinic' component={Clinic}></Route>
-                </Auxiliary>
-              );
-
-            break;
-        case 'adminclinicalcenter':
-            page = (
-                <Auxiliary>
-                    <UserInfo name={name} lastname={lastname} role={role} />
-                    <UserCards>
-                        <UserCard buttonText={"Approve users"} cardText={"Shows all unapproved users"} link = {'/homepage/admin-clinic-center/user-approval'}/>
-                        <UserCard buttonText={"Register clinic"} cardText={"Register new clinic"} link = {'/homepage/admin-clinic-center/register-clinic'}/>
-                        <UserCard buttonText={"Diagnosis registry"} cardText={"Add new diagnosis"} link = {'/homepage/admin-clinic-center/diagnosis-registry'}/>
-                        <UserCard buttonText={"Drugs registry"} cardText={"Add new drug"} link = {'/homepage/admin-clinic-center/drugs-registry'}/>
-                    </UserCards>
-                </Auxiliary>
+                break;
+            case 'adminclinicalcenter':
+                page = (
+                    <Auxiliary>
+                        <UserInfo name={name} lastname={lastname} role={role} />
+                        <UserCards>
+                            <UserCard buttonText={"Approve users"} cardText={"Shows all unapproved users"} link = {'/homepage/admin-clinic-center/user-approval'}/>
+                            <UserCard buttonText={"Register clinic"} cardText={"Register new clinic"} link = {'/homepage/admin-clinic-center/register-clinic'}/>
+                            <UserCard buttonText={"Diagnosis registry"} cardText={"Add new diagnosis"} link = {'/homepage/admin-clinic-center/diagnosis-registry'}/>
+                            <UserCard buttonText={"Drugs registry"} cardText={"Add new drug"} link = {'/homepage/admin-clinic-center/drugs-registry'}/>
+                        </UserCards>
+                    </Auxiliary>
+                );
+                functions = (
+                  <Auxiliary>
+                    <Route path='/homepage/admin-clinic-center/user-approval' component={UserApproval}></Route>
+                    <Route path='/homepage/admin-clinic-center/register-clinic' component={RegisterClinic}></Route>
+                    <Route path='/homepage/admin-clinic-center/diagnosis-registry' component={DiagnosisRegistry}></Route>
+                    <Route path='/homepage/admin-clinic-center/drugs-registry' component={DrugsRegistry}></Route>
+                  </Auxiliary>
+                );
+                break;
+            default:
+                page = <h1>Nije logovan!</h1>
+                break;
+        }
+    } else {
+        page = (
+                <UserPasswordChangeForm/>
             );
-            functions = (
-              <Auxiliary>
-                <Route path='/homepage/admin-clinic-center/user-approval' component={UserApproval}></Route>
-                <Route path='/homepage/admin-clinic-center/register-clinic' component={RegisterClinic}></Route>
-                <Route path='/homepage/admin-clinic-center/diagnosis-registry' component={DiagnosisRegistry}></Route>
-                <Route path='/homepage/admin-clinic-center/drugs-registry' component={DrugsRegistry}></Route>
-              </Auxiliary>
-            );
-            break;
-        default:
-            page = <h1>Nije logovan!</h1>
-            break;
     }
 
     return (
