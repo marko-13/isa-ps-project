a5b28bf79b70fdee24686c3673491767a293bf6e
kresovicmilan <kresovich.milan@gmail.com>
2020-01-11 17:30:02 +0100

    Add repository for clinic administrator

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/AdminClinicRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/AdminClinicRepository.java
@@ -1,4 +1,7 @@
 package com.proj.medicalClinic.repository;
 
-public interface AdminClinicRepository {
+import com.proj.medicalClinic.model.AdminClinic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminClinicRepository extends JpaRepository<AdminClinic, Long> {
 }
