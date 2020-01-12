73ce972b120c6de2b5f62671127c0adbc2c0c325
kresovicmilan <kresovich.milan@gmail.com>
2020-01-12 21:11:30 +0100

    Adding neccessary imports

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/ClinicController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/ClinicController.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clinics", produces = MediaType.APPLICATION_JSON_VALUE)
