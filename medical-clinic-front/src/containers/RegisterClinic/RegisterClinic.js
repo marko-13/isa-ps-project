2477ef77e790e053f308bcfccf9329b0a9196495
kresovicmilan <kresovich.milan@gmail.com>
2020-01-13 01:52:30 +0100

    Making fields required in form for registering new clinic

--- medical-clinic-front/src/containers/RegisterClinic/RegisterClinic.js
+++ medical-clinic-front/src/containers/RegisterClinic/RegisterClinic.js
@@ -36,39 +36,43 @@ class RegisterClinic extends Component {
 		return(
 			<div className="col-md-7 login-form-1" style={{margin: 'auto'}}>
                         <h3>Register new clinic</h3>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Name of the clinic *"
-                                value={this.state.name}
-                                onChange={(event) => this.setState({ name: event.target.value })}
-                            />
-                        </div>
+                        <br/>
+                        <form onSubmit = {this.registerClinic} >
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Name of the clinic *"
+                                    value={this.state.name}
+                                    onChange={(event) => this.setState({ name: event.target.value })}
+                                    required
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Address of the clinic *"
-                                value={this.state.address}
-                                onChange={(event) => this.setState({ address: event.target.value })}
-                            />
-                        </div>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Address of the clinic *"
+                                    value={this.state.address}
+                                    onChange={(event) => this.setState({ address: event.target.value })}
+                                    required
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Description of the clinic *"
-                                value={this.state.description}
-                                onChange={(event) => this.setState({ description: event.target.value })}
-                            />
-                        </div>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Description of the clinic *"
+                                    value={this.state.description}
+                                    onChange={(event) => this.setState({ description: event.target.value })}
+                                    required
+                                />
+                            </div>
 
-                        <div className="form-group" style={{float: 'right', paddingRight: '2%'}}>
-                            <button className="btnSubmit" onClick = {this.registerClinic} style={{margin: '20%', width: '150%', padding: '20%'}}>Register</button>
-                        </div>
+                            <button className="btnSubmit" style={{width: '15%', float: 'right'}} type = "submit">Register</button>
+                        </form>
             </div>
 		)
 	}
