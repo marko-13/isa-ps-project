5c1d8050075d7a92b2d46f7b2039fa707c5f80dc
bokykisac <dervy97@gmail.com>
2020-01-17 12:57:13 +0100

    Adding endpoint for gettind all services from clinic in service controller

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/ServiceController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/ServiceController.java
@@ -1,4 +1,31 @@
 package com.proj.medicalClinic.controller;
 
+import com.proj.medicalClinic.dto.ServiceDTO;
+import com.proj.medicalClinic.service.ServiceService;
+import com.proj.medicalClinic.service.implementation.ServiceServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/service", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ServiceController {
+
+    @Autowired
+    private ServiceService serviceService;
+
+    @RequestMapping("/getAll")
+    @PreAuthorize("hasAuthority('ADMINCLINIC')")
+    public ResponseEntity<?> getAllFromClinic(){
+        List<ServiceDTO> serviceDTOS = serviceService.getAllFromClinic();
+        return new ResponseEntity<>(serviceDTOS, HttpStatus.OK);
+    }
+
 }
