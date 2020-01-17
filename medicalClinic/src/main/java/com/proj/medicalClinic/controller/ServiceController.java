4ec42c2e11225da04f163fa630e025b6ffc3d94e
bokykisac <dervy97@gmail.com>
2020-01-17 20:27:05 +0100

    Adding endpoint for getting all services in service controller

--- medicalClinic/src/main/java/com/proj/medicalClinic/controller/ServiceController.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/controller/ServiceController.java
@@ -1,6 +1,8 @@
 package com.proj.medicalClinic.controller;
 
 import com.proj.medicalClinic.dto.ServiceDTO;
+import com.proj.medicalClinic.exception.NotExistsException;
+import com.proj.medicalClinic.exception.ResourceConflictException;
 import com.proj.medicalClinic.service.ServiceService;
 import com.proj.medicalClinic.service.implementation.ServiceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,11 +24,16 @@ public class ServiceController {
     @Autowired
     private ServiceService serviceService;
 
-    @RequestMapping("/getAll")
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ADMINCLINIC')")
     public ResponseEntity<?> getAllFromClinic(){
-        List<ServiceDTO> serviceDTOS = serviceService.getAllFromClinic();
-        return new ResponseEntity<>(serviceDTOS, HttpStatus.OK);
+        try {
+            List<ServiceDTO> serviceDTOS = serviceService.getAllFromClinic();
+            return new ResponseEntity<>(serviceDTOS, HttpStatus.OK);
+        }catch (NotExistsException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
