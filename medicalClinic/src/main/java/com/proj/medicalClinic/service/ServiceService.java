dbdadf3c873823024ecbc3d7fc024280d23e3d65
bokykisac <dervy97@gmail.com>
2020-01-17 12:56:25 +0100

    Adding method for getting all services from certain clinic in service service

--- medicalClinic/src/main/java/com/proj/medicalClinic/service/ServiceService.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/ServiceService.java
@@ -1,4 +1,11 @@
 package com.proj.medicalClinic.service;
 
+import com.proj.medicalClinic.dto.ServiceDTO;
+
+import java.util.List;
+
 public interface ServiceService {
+
+    List<ServiceDTO> getAllFromClinic();
+
 }
--- medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/ServiceServiceImpl.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/service/implementation/ServiceServiceImpl.java
@@ -1,4 +1,25 @@
 package com.proj.medicalClinic.service.implementation;
 
-public class ServiceServiceImpl {
+import com.proj.medicalClinic.dto.ServiceDTO;
+import com.proj.medicalClinic.repository.ServiceRepository;
+import com.proj.medicalClinic.service.ServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceServiceImpl implements ServiceService {
+
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    @Override
+    public List<ServiceDTO> getAllFromClinic() {
+        // nadji administratora klinike i sacuvaj njegov clinicID
+       // serviceRepository.findAllByClinic(clinicId).orElse(null);
+        return null;
+    }
+
+
 }
