945caaaa91dd7714dc4a1987bde8000fb706f0fb
bokykisac <dervy97@gmail.com>
2020-01-19 15:00:07 +0100

    Adding table of all medical exams

--- medical-clinic-front/src/components/Tables/ExaminationsAll/ExaminationsAll.js
+++ medical-clinic-front/src/components/Tables/ExaminationsAll/ExaminationsAll.js
@@ -3,22 +3,43 @@ import axios from '../../../axios';
 import ReactTable from 'react-table-6';
 
 import Button from '../../../components/UI/Button/Button';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+import Modal from '../../../components/UI/Modal/Modal';
+import EditExam from '../../Forms/EditExam/EditExam';
 
 class ExaminationsAll extends Component {
 
     state = {
-        exams: null
+        exams: null,
+        modalOpen: false,
+        selectedExam: null
     }
 
     componentDidMount() {
+       this.getAllExams();
+    }
 
+    closeModalHandler = () => {
+        this.setState({modalOpen: false});
+    }
+
+    openModalHandler = (exam) => {
+        this.setState({selectedExam: exam})
+        this.setState({modalOpen: true});
     }
 
     getAllExams = () => {
-        
+        axios.get("service/getAll")
+        .then(res => this.setState({exams: res.data}))
+        .catch(err => {
+            console.log(err);
+            this.setState({exams: null});
+        });
     }
 
     render() {
+
+        console.log(this.state.selectedExam);
 
         let table = null;
 
@@ -28,7 +49,7 @@ class ExaminationsAll extends Component {
             }
             else {
                 const columns = [{
-                    Header: 'Requests for leave of absence',
+                    Header: 'List of all medical examinations',
                     columns: [
                         {
                             id: 'name',
@@ -43,7 +64,7 @@ class ExaminationsAll extends Component {
                         {
                             Header: "",
                             Cell: ({ original }) => (
-                                <center><Button type='black'>Edit</Button></center>),
+                                <center><Button type='black' click={() => this.openModalHandler(original)}>Edit</Button></center>),
                             filterable: false,
                             sortable: false
                         },
@@ -63,10 +84,8 @@ class ExaminationsAll extends Component {
                         className="-striped"
                         pageSize={7}
                         filterable={true}
-                        style={{ width: '85vw' }}
                         defaultFilterMethod={(filter, row, column) => {
                             const id = filter.pivotId || filter.id
-                            console.log(row[id]);
                             return row[id] !== undefined ? String(row[id]).toLowerCase().includes(filter.value.toLowerCase()) : true
                         }}
                         pageSize={(this.state.exams.length > 7) ? 7 : this.state.exams.length}
@@ -77,7 +96,14 @@ class ExaminationsAll extends Component {
             table = <h1>Something is not right.</h1>
         }
 
-        return table;
+        return (
+            <Auxiliary>
+                {table}
+                <Modal show={this.state.modalOpen} modalClosed={this.closeModalHandler}>
+                    <EditExam exam={this.state.selectedExam} closeModal={this.closeModalHandler}/>
+                </Modal>
+            </Auxiliary>
+        );
     }
 }
 
