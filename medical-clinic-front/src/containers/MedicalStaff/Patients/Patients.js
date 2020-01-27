c1d690681c059f00490e1a9369ade7d3703892a3
bokykisac <dervy97@gmail.com>
2020-01-28 00:29:04 +0100

    Updating patients component

--- medical-clinic-front/src/containers/MedicalStaff/Patients/Patients.js
+++ medical-clinic-front/src/containers/MedicalStaff/Patients/Patients.js
@@ -3,14 +3,14 @@ import { Route } from 'react-router-dom';
 
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import PatientsTable from '../../../components/Tables/PatientsTable/PatientsTable';
-import UserInfo from '../../../components/Homepage/UserInfo/UserInfo';
+import MedicalHistory from './MedicalHistory/MedicalHistory';
 
 class Patients extends Component {
     render() {
         return (
             <Auxiliary>
                 <PatientsTable/>
-                <Route path='/homepage/doctor/patients/medical-history' render={() => <h1>MED HIS</h1>}></Route>
+                <Route path={this.props.match.path + '/medical-history'} component={MedicalHistory}></Route>
                 <Route path='/homepage/doctor/patients/start-exam' render={() => <h1>EXAM</h1>}></Route>        
             </Auxiliary>
         );
