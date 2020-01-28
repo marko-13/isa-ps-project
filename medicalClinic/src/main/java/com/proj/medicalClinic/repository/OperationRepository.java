2fd9b1ece5efa88d75c5f5f7096a4b3002b98ea8
kresovicmilan <kresovich.milan@gmail.com>
2020-01-28 21:31:31 +0100

    Find all operations that contain specific doctor in theirs list of assigned doctors

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/OperationRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/OperationRepository.java
@@ -1,4 +1,14 @@
 package com.proj.medicalClinic.repository;
 
-public interface OperationRepository {
+import com.proj.medicalClinic.model.Doctor;
+import com.proj.medicalClinic.model.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+    List<Operation> findAllByDoctorsContaining(Doctor doctor);
 }
