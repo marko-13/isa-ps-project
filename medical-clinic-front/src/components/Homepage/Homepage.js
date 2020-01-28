969a5eab13be2cf73347dd6e05bbadae64e62c02
kresovicmilan <kresovich.milan@gmail.com>
2020-01-28 22:10:06 +0100

    Adding neccessary imports

--- medical-clinic-front/src/components/Homepage/Homepage.js
+++ medical-clinic-front/src/components/Homepage/Homepage.js
@@ -18,8 +18,9 @@ import RegisterClinic from '../../containers/RegisterClinic/RegisterClinic';
 import ShowAppointments from '../Appointment/ShowAppointments/ShowAppointments.js';
 import DiagnosisRegistry from '../../containers/DiagnosisRegistry/DiagnosisRegistry';
 import DrugsRegistry from '../../containers/DrugsRegistry/DrugsRegistry';
-import RegisterClinicAdministrator from '../../containers/RegisterClinicAdministrator/RegisterClinicAdministrator'
-import RegisterAdminClinicalCenter from '../../containers/RegisterAdminClinicalCenter/RegisterAdminClinicalCenter'
+import RegisterClinicAdministrator from '../../containers/RegisterClinicAdministrator/RegisterClinicAdministrator';
+import RegisterAdminClinicalCenter from '../../containers/RegisterAdminClinicalCenter/RegisterAdminClinicalCenter';
+import WorkSchedule from '../../containers/WorkSchedule/WorkSchedule';
 import ShowMedicalStaff from '../MedicalStaff/ShowMedicalStaff/ShowMedicalStaff.js';
 import Doctors from '../../containers/ClinicAdministrator/Doctors/Doctors';
 import Clinic from '../../containers/ClinicAdministrator/Clinic/Clinic';
