9bca46b751f6363992a8061c8f1501dc74335411
bokykisac <dervy97@gmail.com>
2020-02-01 22:09:01 +0100

    Adding cron method for automatically adding rooms to appointments which havent been assigned a room

---
+++ medicalClinic/src/main/java/com/proj/medicalClinic/cron/AutoScheduler.java
@@ -0,0 +1,20 @@
+package com.proj.medicalClinic.cron;
+
+import com.proj.medicalClinic.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutoScheduler {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    //za svake tri sekunde cron = "0/3 * * ? * *"
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void addRoomsWithCrone() {
+       appointmentService.cronAddRooms();
+    }
+
+}
