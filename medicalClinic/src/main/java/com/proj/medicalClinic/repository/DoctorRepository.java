038d461da926e5bb4f5768ce461292144849d351
bokykisac <dervy97@gmail.com>
2020-02-01 22:10:37 +0100

    Adding methods for getting all doctors that have certain examination and getting all doctors that are not deleted and belogns to certain clinic

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/DoctorRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/DoctorRepository.java
@@ -1,10 +1,12 @@
 package com.proj.medicalClinic.repository;
 
+import com.proj.medicalClinic.model.Clinic;
 import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.model.Examination;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.print.Doc;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +14,8 @@ import java.util.Optional;
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
     List<Doctor> findAllByDeletedNot(boolean deleted);
+    List<Doctor> findAllByExaminations(Examination examination);
+    List<Doctor> findAllByClinicAndDeletedNot(Clinic clinic, boolean deleted);
     
     @Query(
             value = "SELECT * FROM app_user d WHERE d.id = ?1",
