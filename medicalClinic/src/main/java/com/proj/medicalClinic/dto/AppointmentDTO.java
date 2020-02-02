faba99716b578543ca7c5bb0c20cabc08e0ec128
kresovicmilan <kresovich.milan@gmail.com>
2020-02-02 17:31:03 +0100

    Differentianting contructor for operation and examination class

--- medicalClinic/src/main/java/com/proj/medicalClinic/dto/AppointmentDTO.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/dto/AppointmentDTO.java
@@ -62,14 +62,102 @@ public class AppointmentDTO {
 
             this.service = examination.getService().getType();
 
-            if(examination.getDoctors().isEmpty()){
+            if(examination.getNurse() == null){
+                this.nurse = null;
+            }else{
+                this.nurse = examination.getNurse().getId();
+            }
+
+            if(examination.getPatient() == null){
+                this.patient = "No patient";
+            }else{
+                this.patient = examination.getPatient().getName() + " " + examination.getPatient().getLastName();
+            }
+
+            if (examination.getMReport() != null) {
+                this.medicalReport = new MedicalReportDTO(examination.getMReport());
+            } else {
+                this.medicalReport = null;
+            }
+
+            if (examination.getDoctors() != null) {
+                this.doctors = examination.getDoctors().stream().map(
+                        s -> new DoctorDTO(s)
+                ).collect(Collectors.toList());
+            } else {
                 this.doctors = null;
             }
-            else{
-                List<Doctor> doctors = examination.getDoctors();
-                for(Doctor d : doctors){
-                    this.doctors.add(d.getId());
-                }
+        }
+        else{
+            Operation operation = (Operation) a;
+
+            this.date = operation.getDate();
+            this.duration = operation.getDuration();
+
+            this.fastExam = "Regular";
+
+            this.clinic = operation.getClinic().getName();
+            if (operation.getOperationRoom() == null){
+                this.operationRoom = "";
+            }else {
+                this.operationRoom = operation.getOperationRoom().getName();
+            }
+
+            this.service = operation.getService().getType();
+
+            if (operation.getDoctors() != null) {
+                this.doctors = operation.getDoctors().stream().map(
+                        s -> new DoctorDTO(s)
+                ).collect(Collectors.toList());
+            } else {
+                this.doctors = null;
+            }
+
+            this.nurse = null;
+
+            if(operation.getPatient() == null){
+                this.patient = "No patient";
+            }else{
+                this.patient = operation.getPatient().getName() + " " +  operation.getPatient().getLastName();
+            }
+
+            this.medicalReport = null;
+        }
+    }
+
+    public AppointmentDTO(Appointment a, boolean as){
+        this.type = a.getClass().getAnnotation(DiscriminatorValue.class).value();
+        this.doctors = new ArrayList<>();
+        this.id = a.getId();
+        this.assigned = as;
+
+        if(this.type.equals("EX")){
+            Examination examination = (Examination) a;
+            this.date = examination.getDate();
+            this.duration = examination.getDuration();
+            this.held = examination.isHeld();
+
+            if(examination.isFast()){
+                this.fastExam = "Fast";
+            }else{
+                this.fastExam = "Regular";
+            }
+
+            this.clinic = examination.getClinic().getName();
+            if(examination.getOperationRoom() == null){
+                this.operationRoom = "";
+            }else {
+                this.operationRoom = examination.getOperationRoom().getName();
+            }
+
+            this.service = examination.getService().getType();
+
+            if (examination.getDoctors() != null) {
+                this.doctors = examination.getDoctors().stream().map(
+                        s -> new DoctorDTO(s)
+                ).collect(Collectors.toList());
+            } else {
+                this.doctors = null;
             }
 
             if(examination.getNurse() == null){
