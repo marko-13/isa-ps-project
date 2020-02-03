cc25f5c8861e66d63810a5e5d3a43b675f47ceaa
bokykisac <dervy97@gmail.com>
2020-02-03 19:05:37 +0100

    Adding form for choosing available doctors

--- medical-clinic-front/src/components/Forms/ChooseAvailableDoctorsForm/ChooseAvailableDoctorsForm.js
+++ medical-clinic-front/src/components/Forms/ChooseAvailableDoctorsForm/ChooseAvailableDoctorsForm.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { withRouter } from "react-router";
 
 import classes from './ChooseAvailableDoctorsForm.module.css';
+import {Typeahead} from 'react-bootstrap-typeahead';
+import Button from '../../UI/Button/Button';
 
 class ChooseAvailableDoctorsForm extends Component {
 
     state = {
-        availableDoctors: []
+        availableDoctors: [],
+        selectedDoctor: null,
+        errorHidden: true
     }
 
     componentDidMount() {
@@ -16,10 +21,7 @@ class ChooseAvailableDoctorsForm extends Component {
             appId: this.props.appId,
             roomId: this.props.roomId
         }
-
-        console.log(data);
         
-
         axios.post("/doctor/getAllAvailable", data)
             .then(res => {
                 console.log(res.data);
@@ -28,18 +30,65 @@ class ChooseAvailableDoctorsForm extends Component {
             .catch(err => console.log(err));
     }
 
+    //doctor => dobijamo listu doktoru, ako zelimo selektovanog pristupamo sa doktor[0]
+    onSelectChange = doctor => {
+
+        if(doctor !== null || doctor !== undefined){
+            this.setState({selectedDoctor: doctor[0]});
+        }
+     
+    }
+
+    onAddDoctorHandler = () => {
+        if(this.state.selectedDoctor === null || this.state.selectedDoctor === undefined){
+            this.setState({errorHidden: false});
+            return;
+        }
+
+        const sendDoctor = {
+            appId: this.props.appId,
+            roomId: this.props.roomId,
+            doctorId: this.state.selectedDoctor.id
+        }
+
+        axios.post("/appointment/changeDoctorAndAddRoomToAppointment", sendDoctor)
+            .then(res => {
+                alert(`Doctor ${this.state.selectedDoctor.name} ${this.state.selectedDoctor.lastname} has been added sucessfully!`);
+                this.props.history.push('/homepage/adminc-clinic');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     render() {
+
+        console.log(this.state.selectedDoctor);
 
         return (
             <div>
-                <div>
+                <div className={classes.Header}>
                     <h2>Select new doctor for appointment</h2>
                 </div>
-                <h2>Doktor isn't available for this date, please choose another doctor.</h2>
-                <h1>LISTA DOKTORA</h1>
+                <h5>Doktor isn't available for this date, please choose another doctor.</h5>
+                <br></br>
+                <h4>Available doctors</h4>
+                <Typeahead 
+                    id = "showdoctors" 
+                    placeholder="Choose a doctor..." 
+                    onChange={(selected) => this.onSelectChange(selected)}
+                    labelKey={option => `${option.name} ${option.lastname}`}
+                    options = {this.state.availableDoctors}
+                  />
+                  <br></br>
+                  <p hidden={this.state.errorHidden} className={classes.Error}>Please select a doctor!</p>
+                  <div className={classes.Buttons}>
+                      <Button click={this.props.closeModa}>Back</Button>
+                      <Button click={this.onAddDoctorHandler}>Add doctor</Button>
+                  </div>
             </div>
         );
     }
 }
 
-export default ChooseAvailableDoctorsForm;
+export default withRouter(ChooseAvailableDoctorsForm);
