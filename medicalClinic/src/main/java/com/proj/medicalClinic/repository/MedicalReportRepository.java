acb3c3677f2040683b385b76d453254988b3baef
kresovicmilan <kresovich.milan@gmail.com>
2020-02-04 05:53:02 +0100

    Create query for saving medical report to database

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/MedicalReportRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/MedicalReportRepository.java
@@ -5,6 +5,8 @@ import com.proj.medicalClinic.model.MedicalHistory;
 import com.proj.medicalClinic.model.MedicalReport;
 import com.proj.medicalClinic.model.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,6 +18,13 @@ public interface MedicalReportRepository extends JpaRepository<MedicalReport, Lo
 
     List<MedicalReport> findAllByExamination(Examination examination);
     List<MedicalReport> findAllByPrescription(Prescription prescription);
+    MedicalReport findByExamination(Examination examination);
     List<MedicalReport> findAllByMedicalHistory(MedicalHistory medicalHistory);
+    Optional<MedicalReport> findAllById(Long id);
 
+    @Modifying
+    @Query(
+            value = "INSERT INTO medical_report (exam_description, medical_history_id, prescription_id, examination_id) VALUES (?1, ?2, ?3, ?4)",
+            nativeQuery = true)
+    void saveMedicalReport(String examDescription, Long medicalHistoryId, Long prescriptionId, Long examinationId);
 }
