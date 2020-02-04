1a45c22fc6c5d024be4e988327e213518348f12d
kresovicmilan <kresovich.milan@gmail.com>
2020-02-04 05:53:51 +0100

    Create method for finding nurse by their examinations

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/NurseRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/NurseRepository.java
@@ -1,4 +1,11 @@
 package com.proj.medicalClinic.repository;
 
-public class NurseRepository {
+import com.proj.medicalClinic.model.Examination;
+import com.proj.medicalClinic.model.Nurse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NurseRepository extends JpaRepository<Nurse, Long> {
+    Nurse findByExaminations(Examination examination);
 }
