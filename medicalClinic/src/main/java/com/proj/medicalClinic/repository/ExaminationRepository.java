85245dc95ec8efe209afb5564aa1848db47c3d72
marko-13 <50908634+marko-13@users.noreply.github.com>
2020-02-04 18:15:01 +0100

    Added query that returns all examinations that are being held between two given dates Added query that returns all examinations

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/ExaminationRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/ExaminationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,4 +15,8 @@ import java.util.UUID;
 @Repository
 public interface ExaminationRepository extends JpaRepository<Examination, Long> {
     List<Examination> findAllByDoctorsContaining(Doctor doctor);
+
+    List<Examination> findAllByDateBetween(Date d1, Date d2);
+
+    List<Examination> findAll();
 }
