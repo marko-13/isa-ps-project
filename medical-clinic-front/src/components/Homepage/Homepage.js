b3c9115e81492a3f46e3eff88b52a3b8b96bb3be
kresovicmilan <kresovich.milan@gmail.com>
2020-02-05 17:41:13 +0100

    Add route to start an exam

--- medical-clinic-front/src/components/Homepage/Homepage.js
+++ medical-clinic-front/src/components/Homepage/Homepage.js
@@ -75,6 +75,7 @@ const Homepage = (props) => {
                         <Route path='/homepage/nurse/prescriptions' component={Prescriptions}></Route>
                         <Route path='/homepage/nurse/patients' component={Patients}></Route>
                         <Route path='/homepage/patient/clinics' component={null}></Route>
+                        <Route path='/homepage/start-exam' component={StartExam} />
                       </Auxiliary>
                     );
                     break;
