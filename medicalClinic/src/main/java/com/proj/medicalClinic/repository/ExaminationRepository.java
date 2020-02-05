1b2c25e6023dc75f915dfaa2409cb2f7a49f8458
kresovicmilan <kresovich.milan@gmail.com>
2020-02-05 17:34:36 +0100

    Adding neccessary imports

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/ExaminationRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/ExaminationRepository.java
@@ -2,6 +2,8 @@ package com.proj.medicalClinic.repository;
 import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.model.Examination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
