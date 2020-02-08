98ceb19cea95b55ff30992b28b9c6036485cde3a
kresovicmilan <kresovich.milan@gmail.com>
2020-02-08 03:31:18 +0100

    Add necessary imports

--- medicalClinic/src/main/java/com/proj/medicalClinic/repository/LeaveRepository.java
+++ medicalClinic/src/main/java/com/proj/medicalClinic/repository/LeaveRepository.java
@@ -2,6 +2,7 @@ package com.proj.medicalClinic.repository;
 
 import com.proj.medicalClinic.model.Doctor;
 import com.proj.medicalClinic.model.Leave;
+import com.proj.medicalClinic.model.Nurse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
